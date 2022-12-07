2901ffd9effb653f9967980c3907339b07cc24d5
Jason Steffener - laptop <steffener@gmail.com>
2022-12-07 10:24:13 -0500

    Cant figure it out yet

--- config/SRT_Setup.js
+++ config/SRT_Setup.js
@@ -31,7 +31,7 @@ var PracticeWordList = [
 var ResponseButtons = [1,0]
 
 var NBlocks = 5
-var TimePerWord = 1000 // milliseconds
+var TimePerWord = 200 // milliseconds
 var FixationTimeBetweenWords = 200 // milliseconds
 var RecallInstructions = 'Please repeat the entire word list'
 var GetReadyInstructions = 'Get ready for the next trial'
@@ -41,6 +41,12 @@ var Instructions = [
 	{'page': 'For the words that were not recalled during the trial, they will be repeated and you will be asked to recall the entire list of words again.'},
 	{'page': '(including the original list of words and the words you may have forgot). This procedure is repeated for 5 trials. Press Next to begin'},
 	]
+
+var Block01List = [0,1,2,3,4,5,6,7,8,9,10,11]
+var Block02List = [0,1,2,3,4,5,6,7,8,9,10,11]
+var Block03List = [0,1,2,3,4,5,6,7,8,9,10,11]
+var Block04List = [0,1,2,3,4,5,6,7,8,9,10,11]
+
 var CreateSimpleWordList = function(WordList) {
 	var SimpleList = []
 	for (var i = 0; i < WordList.length; i++) {
@@ -49,20 +55,56 @@ var CreateSimpleWordList = function(WordList) {
 	return SimpleList
 }
 
+// This list is used for checking responses to see if the spoke word is part of 
+// the full list
 var SimpleList = CreateSimpleWordList(WordList)
-var myFunction2 = function(tag) {
+
+//ThisBlockList = [0,1,2,3,4,5,6,7,8,9,10,11]
+var FindRecalledWords01 = function(tag) {
     console.log(tag)
-    HeardList.push(tag)    
     // See if this item is in the word list
     if (SimpleList.includes(tag.toUpperCase())) 
     {
-      // find the words index in teh list
-      console.log(SimpleList.indexOf(tag.toUpperCase()))
+      // find the words index in the list
       console.log('Found One')
       // remove the item
-      SimpleList.splice(SimpleList.indexOf(tag.toUpperCase()),1)
-      console.log(SimpleList)
+      //SimpleList.splice(SimpleList.indexOf(tag.toUpperCase()),1)
+      Block02List[SimpleList.indexOf(tag.toUpperCase())]=-99
+      //console.log(SimpleList)
+      console.log(Block02List)
     }
   }
 
-var HeardList = []
+var FindRecalledWords02 = function(tag) {
+    console.log(tag)
+    // See if this item is in the word list
+    if (SimpleList.includes(tag.toUpperCase())) 
+    {
+      // find the words index in the list
+      console.log('Found One')
+      // remove the item
+      //SimpleList.splice(SimpleList.indexOf(tag.toUpperCase()),1)
+      Block03List[SimpleList.indexOf(tag.toUpperCase())]=-99
+      //console.log(SimpleList)
+      console.log(Block03List)
+    }
+  }
+
+
+var FindRecalledWords03 = function(tag) {
+    console.log(tag)
+    // See if this item is in the word list
+    if (SimpleList.includes(tag.toUpperCase())) 
+    {
+      // find the words index in the list
+      console.log('Found One')
+      // remove the item
+      //SimpleList.splice(SimpleList.indexOf(tag.toUpperCase()),1)
+      Block04List[SimpleList.indexOf(tag.toUpperCase())]=-99
+      //console.log(SimpleList)
+      
+    }
+  }
+
+
+
--- functions/Experiments/SRT_experiment.js
+++ functions/Experiments/SRT_experiment.js
@@ -11,7 +11,16 @@ the computer.
 
 // =======================================================================
 // Define internal variables
+var CurrentList = [11]
 var timeline = [];
+//var data = [];
+//data.WordList = WordList;
+// This is a list of the indices of words to present
+// It should be updated after every recall event
+//data.Block01List = [0,1,2,3,4,5,6,7,8,9,10,11]
+//data.Block02List = [0,1,2,3,4,5,6,7,8,9,10,11]
+//data.Block03List = [0,1,2,3,4,5,6,7,8,9,10,11]
+//data.Block04List = [0,1,2,3,4,5,6,7,8,9,10,11]
 
 // =======================================================================
 var enter_fullscreen = {
@@ -36,6 +45,7 @@ var Stimulus = {
     stimulus: function()
       {
         var Stim = jsPsych.timelineVariable('Word')
+        console.log(Stim)
         return Stim
       },
     choices: [], 
@@ -43,43 +53,157 @@ var Stimulus = {
     post_trial_gap: 0,
     trial_duration: TimePerWord,
     prompt: StroopWordPrompt, //Add this to config file
+    on_finish: function(data) {
+      data.task = 'word'
+    }
   };
 
-var RecallRequest = {
+var RecallRequest01 = {
     type: jsPsychHtmlButtonResponseTouchscreen,
-    stimulus: "Recall this full list",
+    stimulus: "Please, recall the full list",
     choices: ['Next'], 
     margin_horizontal: GapBetweenButtons,
     post_trial_gap: 0,
     prompt: StroopWordPrompt, //Add this to config file
     on_start: function(SimpleList) {
-        const commands2 = {'*search': myFunction2};
+      //data.ThisBlockList = [0,1,2,3,4,5,6,7,8,9,10,11]
+      const commands01 = {'*search': FindRecalledWords01};
       annyang.start({autorestart: false, continuous: true});
-      annyang.addCommands(commands2);
-      console.log('Started')
+      annyang.addCommands(commands01);
+      //console.log('Started')
+    },
+    on_finish: function(data){
+      data.RecallList = Block02List
+      data.task = 'Recall'
+    },
+  }
+  
+var RecallRequest02 = {
+    type: jsPsychHtmlButtonResponseTouchscreen,
+    stimulus: "Please, recall the full list",
+    choices: ['Next'], 
+    margin_horizontal: GapBetweenButtons,
+    post_trial_gap: 0,
+    prompt: StroopWordPrompt, //Add this to config file
+    on_start: function(SimpleList) {
+      //data.ThisBlockList = [0,1,2,3,4,5,6,7,8,9,10,11]
+      const commands02 = {'*search': FindRecalledWords02};
+      annyang.start({autorestart: false, continuous: true});
+      annyang.addCommands(commands02);
+      //console.log('Started')
+    },
+    on_finish: function(data) {
+      data.RecallList = Block03List
+      data.task = 'Recall'
     }
-  };
+  }
 
+var RecallRequest03 = {
+    type: jsPsychHtmlButtonResponseTouchscreen,
+    stimulus: "Please, recall the full list",
+    choices: ['Next'], 
+    margin_horizontal: GapBetweenButtons,
+    post_trial_gap: 0,
+    prompt: StroopWordPrompt, //Add this to config file
+    on_start: function(SimpleList) {
+      //data.ThisBlockList = [0,1,2,3,4,5,6,7,8,9,10,11]
+      const commands03 = {'*search': FindRecalledWords03};
+      annyang.start({autorestart: false, continuous: true});
+      annyang.addCommands(commands03);
+      //console.log('Started')
+
+    },
+    on_finish: function(data) {
+      data.RecallList = Block04List
+      data.task = 'Recall'
+    }
+  }  
 // =======================================================================    
 // Define procedures using the stimuli
 // Define the test procedure which does NOT provide feedback
-  var test_procedure = {
+  var block1 = {
       timeline: [fixation, Stimulus],
-      timeline_variables: WordList,
+      timeline_variables: WordList,  
+      sample: {
+        type: 'custom',
+        fn: function() {
+              function checkResponse(response) {
+                //console.log(response)
+                return response > -99;
+              }
+          var temp = Block01List
+          return temp.filter(checkResponse)
+        }
+      }, 
+  }
+//  function checkResponse(response) {
+//    console.log(response)
+//    return response > -99;
+//  }
+
+var checkResponse = function(response) {
+        return response > -99;
+}
+
+var FindTrials = function() {
+  CurrentList = jsPsych.data.get().filter({task:'Recall'}).last(1).trials[0].RecallList
+  CurrentList = CurrentList.filter(checkResponse)
+    console.log(CurrentList)
+    return CurrentList  
+}
+  var block2 = {
+      timeline: [fixation, Stimulus],
+      timeline_variables: WordList, 
+      sample: {
+        type: 'custom',
+        fn: function() {
+          return FindTrials;
+        }
+      },
+  };
+
+  var block3 = {
+      timeline: [fixation, Stimulus],
+      timeline_variables: WordList,  
+      sample: {
+        type: 'custom',
+        fn: function() {
+          function checkResponse(response) {
+                //console.log(response)
+                return response > -99;
+              }
+          var temp = Block03List
+          return temp.filter(checkResponse)
+        }
+      } 
+  }
+
+  var recall1 = {
+      timeline: [RecallRequest01],
+      randomize_order: false,
+      repetitions: 1,      
+  }
+  var recall2 = {
+      timeline: [RecallRequest02],
+      randomize_order: false,
+      repetitions: 1, 
+  }
+  var recall3 = {
+      timeline: [RecallRequest03],
       randomize_order: false,
       repetitions: 1, 
   }
 
-  var block_procedure = {
-      timeline: [test_procedure, RecallRequest],
-      randomize_order: false,
-      repetitions: NBlocks, 
-  }
   
 // ======================================================================= 
 // Add procedures to the timeline
-timeline.push(RecallRequest)
-timeline.push(block_procedure);
 
+timeline.push(block1);
+timeline.push(recall1);
+
+timeline.push(block2);
+timeline.push(recall2);
+timeline.push(block3);
+timeline.push(recall3);
 
 
