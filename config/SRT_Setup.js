423f23be0e652fa5cd5f371ac450fe927d53c614
Jason Steffener - laptop <steffener@gmail.com>
2022-12-16 09:13:06 -0500

    fixed the timer

--- config/SRT_Setup.js
+++ config/SRT_Setup.js
@@ -28,7 +28,7 @@ var ResponseButtons = [1,0]
 // The number of immediate recall blocks 
 var NBlocks = 6
 // The time each word is presented on the screen for
-var TimePerWord = 100 // milliseconds
+var TimePerWord = 1000 // milliseconds
 // The amount of time to show a crosshair on the screen between each word
 var FixationTimeBetweenWords = 200 // milliseconds
 
