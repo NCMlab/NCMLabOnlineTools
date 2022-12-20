ce809e037fe8de259d0379f2e8667fe0bebd7453
Jason Steffener - laptop <steffener@gmail.com>
2022-12-20 14:34:21 -0500

    Added animal fluency task

---
+++ functions/TaskSpecificFunctions/AFT_function.js
@@ -0,0 +1,11 @@
+
+
+var RecordSpokenWords = function(tag) {
+    console.log(tag)
+    var response = tag.split(/(\s+)/).filter( function(e) { return e.trim().length > 0; } );
+    // See if this item is in the word list
+    for (var i = 0; i < response.length; i++) {
+	    HeardList.push(response[i])
+	 }
+	 return HeardList
+}
