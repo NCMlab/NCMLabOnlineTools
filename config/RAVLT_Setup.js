06ec992889ec5627d1f2606f6949e9f599d40012
Jason Steffener - laptop <steffener@gmail.com>
2022-12-20 14:20:19 -0500

    Update RAVLT_Setup.js

--- config/RAVLT_Setup.js
+++ config/RAVLT_Setup.js
@@ -23,7 +23,6 @@ var AlternatePronunciationsWordListA = [
 	{'Word':'KNOWS','index':10}
 	]
 
-
 var WordListB = [
 	{'Word':'Desk'},
 	{'Word':'Ranger'},
@@ -45,7 +44,7 @@ var AlternatePronunciationsWordListB = []
 
 var ResponseButtons = [1,0]
 // The number of immediate recall blocks 
-var NBlocks = 2
+var NBlocks = 5
 // The time each word is presented on the screen for
 var TimePerWord = 1000 // milliseconds
 // The amount of time to show a crosshair on the screen between each word
