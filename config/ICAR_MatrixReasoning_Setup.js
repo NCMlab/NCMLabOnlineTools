2b6c821d36f950a05edbab6085cef3f487749e8c
Jason Steffener - laptop <steffener@gmail.com>
2023-01-16 16:10:06 -0500

    Update ICAR_MatrixReasoning_Setup.js

--- config/ICAR_MatrixReasoning_Setup.js
+++ config/ICAR_MatrixReasoning_Setup.js
@@ -3,6 +3,8 @@ var StimulusFolderName = "../assets/MatrixReasoning/ICAR/"
 
 var ICAR_MatrixList = [
   {"stim":"12043","correct":4},
+  {"stim":"12054","correct":1},
+  {"stim":"12055","correct":4},
   {"stim":"12056","correct":5}
  ]
 // how large the options are
@@ -14,7 +16,3 @@ var FixationTimeBetweenTrials = 2000// milliseconds
 
 // used for scoring
 var OptionMapping = '123456'
-
-
-
-  
