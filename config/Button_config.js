b62dc05a7106332a2d1153c2a0f90734d2c30a9b
Jason Steffener - laptop <steffener@gmail.com>
2023-01-17 09:47:31 -0500

    more icons

--- config/Button_config.js
+++ config/Button_config.js
@@ -5,6 +5,7 @@ IconMapping['Color'] = '<img src = "'+IconFolder+'StroopColor.png">'
 IconMapping['ColorWord'] = '<img src = "'+IconFolder+'StroopColorWord.png">'
 IconMapping['TrailsA'] = '<img src = "'+IconFolder+'TrailMakingA.png">'
 IconMapping['TrailsB'] = '<img src = "'+IconFolder+'TrailMakingB.png">'
+IconMapping['ICARMatrixReasoning'] = '<img src = "'+IconFolder+'ICARMatrixReasoning.png">'
 
  function MakeListOfButtonIcons(List)
  {
