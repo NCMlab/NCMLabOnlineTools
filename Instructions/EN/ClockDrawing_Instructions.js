74243f2b503fec8a28f925d4a77d3a3facb7e733
Jason Steffener - laptop <steffener@gmail.com>
2023-01-17 12:01:42 -0500

    added clock drawing task

---
+++ Instructions/EN/ClockDrawing_Instructions.js
@@ -0,0 +1,4 @@
+var Instructions = 'I would like you to draw a clock, put in all the numbers, and set the hands for 10 after 11.'
+
+var WelcomeText = [{'page': 'This is the Clock Drawing Task. Press Next to Continue'}]
+var ThankYouText = [{'page': 'Thank you. Press Next to Continue'}]
---
+++ functions/Experiments/ClockDrawing_experiment.js
@@ -0,0 +1,44 @@
+
+var timeline = []
+
+var trial = {
+  type: jsPsychSketchpad,
+  prompt: Instructions,
+  prompt_location: 'abovecanvas',
+  canvas_width: 600,
+  canvas_height: 600,
+  canvas_border_width: 2
+}
+
+
+  var Instructions = {
+      type: jsPsychHtmlButtonResponseTouchscreen,
+       stimulus: function()
+      {
+        var stim = jsPsych.timelineVariable('page') // Variable in the config file
+        return stim
+      },
+      post_trial_gap: 0,
+      margin_horizontal: GapBetweenButtons,
+      prompt: '',
+      choices: ['Next'], 
+  }
+
+var welcome = {
+      timeline: [Instructions],
+      timeline_variables: WelcomeText,
+      randomize_order: false,
+      repetitions: 1,
+    }
+
+
+  var thank_you = {
+      timeline: [Instructions],
+      timeline_variables: ThankYouText,
+      randomize_order: false,
+      repetitions: 1,
+    }
+timeline.push(welcome)
+timeline.push(trial)
+timeline.push(thank_you)
+
