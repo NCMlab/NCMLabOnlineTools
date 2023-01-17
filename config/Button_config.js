f24fd95b967f9a302e1a31b13e494301179415f1
Jason Steffener - laptop <steffener@gmail.com>
2023-01-17 09:25:17 -0500

    working on button icons

--- config/Button_config.js
+++ config/Button_config.js
@@ -1,5 +1,15 @@
-var IconMapping = [
-	{'Color': 'StroopColor.png'},
-	{'Word': 'StroopWord.png'},
-	{'ColorWord': 'StroopColorWord.png'}
-]
+var IconMapping = {}
+var IconFolder = "../assets/Icons/"
+IconMapping['Word'] = '<img src = "'+IconFolder+'StroopWord.png">'
+IconMapping['Color'] = '<img src = "'+IconFolder+'StroopColor.png">'
+IconMapping['ColorWord'] = '<img src = "'+IconFolder+'StroopColorWord.png">'
+
+ function MakeListOfButtonIcons(List)
+ {
+ 	var options = []
+ 	for ( var i = 0; i < List.length; i++ ) 
+ 	{	
+ 		options.push(IconMapping[List[i]])
+ 	}
+ 	return options
+ }
