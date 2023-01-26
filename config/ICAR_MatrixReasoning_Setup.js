546dfa42ae53e34ee5cfae28d24cd70349218b55
Jason Steffener - laptop <steffener@gmail.com>
2023-01-26 15:23:46 -0500

    Reorganization

--- config/ICAR_MatrixReasoning_Setup.js
+++ config/ICAR_MatrixReasoning_Setup.js
@@ -6,8 +6,10 @@ var ICAR_MatrixList = [
   {"stim":"12044","correct":3},
   {"stim":"12045","correct":5},
   {"stim":"12046","correct":2},
+  {"stim":"12047","correct":2},
   {"stim":"12048","correct":4},
-  {"stim":"12053","correct":1},
+  {"stim":"12050","correct":5},
+  {"stim":"12053","correct":3},
   {"stim":"12054","correct":1},
   {"stim":"12055","correct":4},
   {"stim":"12056","correct":5}
