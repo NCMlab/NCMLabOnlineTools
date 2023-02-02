a44a325a7decafa894c8c823f477f0de6fa9be3d
Jason Steffener - laptop <steffener@gmail.com>
2023-02-02 13:57:03 -0500

    minor changes

--- config/General_Setup.js
+++ config/General_Setup.js
@@ -2,11 +2,9 @@ var FeedbackLength = 400; // This is in milliseconds
 var FeedbackFontSize = "30px";
 var GapBetweenButtons = "10px";
 
-
 var FullScreenMode = false;
 var TextFontSize = "30px";
 var StimulusFontSize = '60px';
-
 
 var SaveLocalFile = false
 
