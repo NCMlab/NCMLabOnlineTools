5177b93f4c2771583de44984d9d3f6c413c69736
Jason Steffener - laptop <steffener@gmail.com>
2023-02-20 09:12:36 -0400

    adding additioninal word recall tasks. Need to add recognition and interference ability into the same code.

---
+++ InstructionsAndStimuli/EN/Stimuli/RAVLT.js
@@ -0,0 +1,50 @@
+// Rey auditory verbal learning test
+
+var WordListA = [
+	{'Word':'Drum'}, 		// 0
+	{'Word':'Curtain'}, 	// 1
+	{'Word':'Bell'},		// 2
+	{'Word':'Coffee'},		// 3
+	{'Word':'School'},		// 4
+	{'Word':'Parent'},		// 5
+	{'Word':'Moon'},		// 6
+	{'Word':'Garden'},		// 7
+	{'Word':'Hat'},			// 8 
+	{'Word':'Farmer'},		// 9
+	{'Word':'Nose'},		// 10
+	{'Word':'Turkey'},		// 11
+	{'Word':'Color'},		// 12
+	{'Word':'House'},		// 13
+	{'Word':'River'},		// 14
+	]
+
+var AlternatePronunciationsWordListA = [
+	{'Word':'COLOUR','index':12},
+	{'Word':'KNOWS','index':10}
+	]
+
+var WordListB = [
+	{'Word':'Desk'},
+	{'Word':'Ranger'},
+	{'Word':'Bird'},
+	{'Word':'Shoe'},
+	{'Word':'Stove'},
+	{'Word':'Mountain'},
+	{'Word':'Glasses'},
+	{'Word':'Towel'},
+	{'Word':'Cloud'},
+	{'Word':'Boat'},
+	{'Word':'Lamb'},
+	{'Word':'Gun'},
+	{'Word':'Pencil'},
+	{'Word':'Church'},
+	{'Word':'Fish'},
+	]
+var AlternatePronunciationsWordListB = []
+var FolderName = '../../assets/SoundFiles/Words/RAVLTSet001/'
+var FileExtension = '.wav'
+
+var NWords = WordListA.length
+
+// The number of immediate recall blocks 
+var NBlocks = 5
---
+++ config/WordRecallGeneral_Setup.js
@@ -0,0 +1,11 @@
+var ResponseButtons = [1,0]
+// The number of immediate recall blocks 
+var NBlocks = 5
+// The time each word is presented on the screen for
+var TimePerWord = 1000 // milliseconds
+// The amount of time to show a crosshair on the screen between each word
+var FixationTimeBetweenWords = 200 // milliseconds
+
+var RecallDuration = 60 // in seconds
+
+
