c6d171a1aadccbe9c5f99037ee01a2f8894fe859
Jason Steffener - laptop <steffener@gmail.com>
2023-03-06 14:11:14 -0400

    Update General_Setup.js

--- config/General_Setup.js
+++ config/General_Setup.js
@@ -1,6 +1,6 @@
 var FeedbackLength = 400; // This is in milliseconds
 var FeedbackFontSize = "30px";
-var GapBetweenButtons = "5px";
+var GapBetweenButtons = "0px";
 
 var FullScreenMode = false;
 var TextFontSize = "30px";
