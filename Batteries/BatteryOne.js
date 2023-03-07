84f342b8ca08c6d72010225f3cb992c28a8b0c30
Jason Steffener - laptop <steffener@gmail.com>
2023-03-07 14:52:44 -0400

    adding Battery

---
+++ Batteries/BatteryOne.js
@@ -0,0 +1,23 @@
+var timeline = []
+var TaskList = []
+TaskList.push('Stroop Color')
+TaskList.push('Stroop Word')
+TaskList.push('Stroop Color/Word')
+var trial1 = {
+  // This displays a series of buttons on the screen for each component of this session. 
+  // The buttons are dynamically created based on what has been completed already
+    type: jsPsychHtmlButtonResponse,
+    stimulus: function() {
+    	var stim = ''
+    	for (var i = 0; i < TaskList.length; i++ ) {
+    		stim += '<p>' + TaskList[i] + '</p>'
+    	}
+    	return stim
+    },
+    prompt: "<p>Stroop</p>", 
+    // This is just a place holder to stop a jsPsych error
+    choices: ['Next'],
+    response_ends_trial: true,
+  };
+
+  timeline.push(trial1)
