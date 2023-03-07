b1dba1d627b0a136f0fbfd512a6181826247f12c
Jason Steffener - laptop <steffener@gmail.com>
2023-03-07 19:23:59 -0400

    working on batteries

---
+++ config/ComponentList.js
@@ -0,0 +1,18 @@
+var ComponentList = []
+const component = {index: 1, name:'Stroop Color', componentID: 32}
+ComponentList.push(component)
+const component = {index: 2, name:'Stroop Word', componentID: 34}
+ComponentList.push(component)
+const component = {index: 3, name:'Stroop Color/Word', componentID: 35}
+ComponentList.push(component)
+const component = {index: 4, name:'Digitspan Forward', componentID: 36}
+ComponentList.push(component)
+const component = {index: 5, name:'Digitspan Backward', componentID: 37}
+ComponentList.push(component)
+
+var BatteryList = []
+const component = {index: 1, name:'Battery001', list: [2,4]}
+BatteryList.push(component)
+
+
+
