3946cec2cd85bb29b0669ef118ef9772bd353017
Jason Steffener - laptop <steffener@gmail.com>
2023-03-13 11:03:41 -0400

    Update BatteryOne.js

--- Batteries/BatteryOne.js
+++ Batteries/BatteryOne.js
@@ -62,7 +62,7 @@ var trial0 = {
       console.log(all_data)
     	// find the battery selected and extract its list of components
     	var ParameterList = BatteryList.find(x => x.index === parseInt(all_data.trials[0].Battery)).ParameterLists
-      console.log("../../config/"+ParameterList[0]+".js")
+      console.log("./../../config/"+ParameterList[0]+".js")
       import {Stroop_Setup} from "../../config/"+ParameterList[0]+".js"
       var list = BatteryList.find(x => x.index === parseInt(all_data.trials[0].Battery)).list
     	// Make a task list of the components of the battery
