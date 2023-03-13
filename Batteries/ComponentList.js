f1dde9446ff003b6d4b640db0fbe730a04d0a9db
Jason Steffener - laptop <steffener@gmail.com>
2023-03-13 14:15:02 -0400

    d

--- Batteries/ComponentList.js
+++ Batteries/ComponentList.js
@@ -6,6 +6,9 @@ ComponentList.push({index: 2, name:'Stroop Color/Word', componentID: 35})
 ComponentList.push({index: 3, name:'Digitspan Forward', componentID: 36})
 ComponentList.push({index: 4, name:'Digitspan Backward', componentID: 37})
 
+
+
+
 var BatteryList = []
 var BatteryDescription = 'This is a description of the battery of tests that will be taken.'
 BatteryList.push({index: 1, 
