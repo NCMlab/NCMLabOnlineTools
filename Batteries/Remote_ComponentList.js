9c36a0a78f38461116e24098057d497836574261
Jason Steffener - laptop <steffener@gmail.com>
2023-04-05 15:14:00 -0400

    Update Remote_ComponentList.js

--- Batteries/Remote_ComponentList.js
+++ Batteries/Remote_ComponentList.js
@@ -3,8 +3,7 @@ var ComponentList = []
 ComponentList.push({name:'Stroop Color', iconFileName:'StroopColor.png'})
 ComponentList.push({name:'Stroop Word', iconFileName:'StroopWord.png'})
 ComponentList.push({name:'Stroop Color/Word', iconFileName:'StroopColorWord.png'})
-ComponentList.push({name:'Trail Making A', iconFileName:'TrailMakingA.png'})
-ComponentList.push({name:'Trail Making B', iconFileName:'TrailMakingB.png'})
+ComponentList.push({name:'Trail Making', iconFileName:'TrailMakingA.png'})
 ComponentList.push({name:'RAVLT', iconFileName:'RAVLT.png'})
 ComponentList.push({name:'ICAR Matrix Reasoning', iconFileName:'ICARMatrixReasoning.png'})
 ComponentList.push({name:'Cube Copy', iconFileName:'CubeCopy.png'})
@@ -17,8 +16,7 @@ ComponentList.push({name:'DigitSpan', iconFileName:'MultiChoice.png'})
 add('Stroop Color', function(){jatos.startComponent(5)});
 add('Stroop Word', function(){jatos.startComponent(6)});
 add('Stroop Color/Word', function(){jatos.startComponent(7)});
-add('Trail Making A', function(){jatos.startComponent(3)});
-add('Trail Making B', function(){jatos.startComponent(4)});
+add('Trail Making', function(){jatos.startComponent(21)});
 add('RAVLT', function(){jatos.startComponent(10)});
 add('ICAR Matrix Reasoning', function(){jatos.startComponent(16)});
 add('Cube Copy', function(){jatos.startComponent(15)});
@@ -34,13 +32,12 @@ var BatteryDescription = 'This is a description of the battery of tests that wil
 
 BatteryList.push({index: 1, 
 	name:'Battery001', 
-	list: ['Digit Span','Cube Copy','Stroop Color','Stroop Word','Stroop Color/Word','Trail Making A','Trail Making B','RAVLT'], 
+	list: ['Digit Span','Cube Copy','Stroop Color','Stroop Word','Stroop Color/Word','Trail Making','RAVLT'], 
 	description: BatteryDescription,
 	ParameterLists: ['DigitSpan_Visual_Forward_Fixed01_Load05','CubeCopy_Default','Stroop_Default',
 		'Stroop_Default',
 		'Stroop_001',
 		'TrailMakingA_Default',
-		'TrailMakingB_Default',
 		'RAVLT_Default'
 		]})
 
