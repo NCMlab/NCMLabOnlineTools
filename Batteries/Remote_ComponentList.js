cff10a907514549f04aa5a76acaf3e3a1296efdc
Jason Steffener - laptop <steffener@gmail.com>
2023-04-05 15:11:06 -0400

    Update Remote_ComponentList.js

--- Batteries/Remote_ComponentList.js
+++ Batteries/Remote_ComponentList.js
@@ -10,7 +10,7 @@ ComponentList.push({name:'ICAR Matrix Reasoning', iconFileName:'ICARMatrixReason
 ComponentList.push({name:'Cube Copy', iconFileName:'CubeCopy.png'})
 ComponentList.push({name:'Likert', iconFileName:'Likert.png'})
 ComponentList.push({name:'MultipleChoice', iconFileName:'MultiChoice.png'})
-
+ComponentList.push({name:'DigitSpan', iconFileName:'MultiChoice.png'})
 // What battery was passed?
 
 // I think I can use the name instead of the index
@@ -23,7 +23,7 @@ add('RAVLT', function(){jatos.startComponent(10)});
 add('ICAR Matrix Reasoning', function(){jatos.startComponent(16)});
 add('Cube Copy', function(){jatos.startComponent(15)});
 add('Likert',function(){jatos.startComponent(19)});
-
+add('Digit Span',function(){jatos.startComponent(20)});
 
 const BatteryHtmlID = 1
 
@@ -32,11 +32,11 @@ var BatteryList = []
 // This is not used yet
 var BatteryDescription = 'This is a description of the battery of tests that will be taken.'
 
-BatteryList.push({index: 3, 
+BatteryList.push({index: 1, 
 	name:'Battery001', 
-	list: ['Cube Copy','Stroop Color','Stroop Word','Stroop Color/Word','Trail Making A','Trail Making B','RAVLT'], 
+	list: ['Digit Span','Cube Copy','Stroop Color','Stroop Word','Stroop Color/Word','Trail Making A','Trail Making B','RAVLT'], 
 	description: BatteryDescription,
-	ParameterLists: ['CubeCopy_Default','Stroop_Default',
+	ParameterLists: ['DigitSpan_Visual_Forward_Fixed01_Load05','CubeCopy_Default','Stroop_Default',
 		'Stroop_Default',
 		'Stroop_001',
 		'TrailMakingA_Default',
@@ -50,7 +50,7 @@ BatteryList.push({index: 2,
 	description: BatteryDescription,
 	ParameterLists: ['Stroop_Default','Stroop_Default']})
 
-BatteryList.push({index: 1, 
+BatteryList.push({index: 3, 
 	name:'List of Questionnaires', 
 	list: ['Likert','Likert','Likert','Likert'], 
 	description: BatteryDescription,
