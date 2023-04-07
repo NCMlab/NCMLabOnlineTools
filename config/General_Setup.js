c3dd0893c204e22e8ea33b8f5597171893aaa18a
Jason Steffener - laptop <steffener@gmail.com>
2023-04-07 12:43:06 -0400

    moved finish button for cube copy, again

--- config/General_Setup.js
+++ config/General_Setup.js
@@ -2,7 +2,7 @@ var FeedbackLength = 400; // This is in milliseconds
 var FeedbackFontSize = "30px";
 var GapBetweenButtons = "0px";
 
-var FullScreenMode = true;
+var FullScreenMode = false;
 var TextFontSize = "30px";
 var StimulusFontSize = '60px';
 
