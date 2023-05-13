3ae6eed9ec4aee99482ffda8de7dfeec101feaeb
Jason Steffener - laptop <steffener@gmail.com>
2023-05-13 08:18:46 -0400

    MR

--- InstructionsAndStimuli/EN/Instructions/MatrixReasoning_Instructions.js
+++ InstructionsAndStimuli/EN/Instructions/MatrixReasoning_Instructions.js
@@ -4,7 +4,7 @@ var MatrixReasoning_Instructions = [
     {'page': 'For this task you will see 3x3 arrays of geometric shapes with one of the nine shapes missing. You are to identify which of six geometric shapes presented below the grid best complete the stimuli.'},
     {'page': '<div>You will see images like this and will need to pick from a series of options (not shown here) which best fit where the question mark is.<br />'+
     'Notice the different shapes, the different colorings, and the different orientations. Use all of this information when making your decisions.'+
-    '</div><div><img src='+BaseFolderName+'InstructionalImage.png alt=Instructional Image></div>'},
+    '</div><div><img src='+BaseFolderName+'InstructionalImage.png alt=Instructional Image></image></div>'},
     {'page': 'You will have ten minutes to complete eleven trials. <div>Press next to begin</div>'}
   ]
 
