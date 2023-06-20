c00b1e879e05bc1d607880a4a9ce8e9e37401060
Jason Steffener - laptop <steffener@gmail.com>
2023-06-20 11:06:37 -0400

    minor battery changes

--- config/ClockDrawing_Setup.js
+++ config/ClockDrawing_Setup.js
@@ -5,5 +5,10 @@ var ClockDrawing_001 = {
 	ShowThankYou: false,
 	ShowInstructions: false, 
 }
-
+var ClockDrawing_CBprotocol = {
+	ShowWelcome: true,
+	ShowThankYou: false,
+	ShowInstructions: false, 
+}
 add('ClockDrawing_001', function(){ ClockDrawing_parameters = ClockDrawing_001});
+add('ClockDrawing_CBprotocol', function(){ ClockDrawing_parameters = ClockDrawing_CBprotocol});
