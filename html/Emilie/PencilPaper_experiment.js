0455be3f149a81f629f8e0dfbff0456cfae6e52e
Jason Steffener <steffener@gmail.com>
2023-11-13 11:35:36 -0500

    PaperPencilWithCommon Parts

--- html/Emilie/PencilPaper_experiment.js
+++ html/Emilie/PencilPaper_experiment.js
@@ -3,7 +3,6 @@ var timeline = [];
 
 /* trial stimuli array for timeline variables */
 
-
 var instructionStimuli = [
     { stimulus: `
         <p>Click on the <strong>pencil</strong>, 
@@ -36,13 +35,15 @@ var preload = {
 var displayImages = {
     type: jsPsychHtmlButtonResponse,
     stimulus: "",
-    choices: [`<div style='float: left;'><img src='img/pencil.png' style='padding: 50px;'></img></div>`,
-    `Next`,
-    `<div style='float: left;'><img src='img/paper.png' style='padding: 50px;'></img></div>`],
+    choices: function() { return [`<div style='float: left;'><img src='img/pencil.png' style='padding: 50px;'></img></div>`,
+        LabelNames.Next,
+        `<div style='float: left;'><img src='img/paper.png' style='padding: 50px;'></img></div>`
+    ]
+    },
     button_html: '<button class="jspsych-btn" style="font-size: 25px; vertical-align: bottom; display: table-cell; border: none;"><strong>%choice%</strong></button>',
     data: {
         task: 'response',
-        correct_response: jsPsych.timelineVariable('correct_response')
+        correct_response: function() { return jsPsych.timelineVariable('correct_response') }
     },
     on_finish: function(data){
         data.correct = data.response === data.correct_response[ind];
@@ -52,42 +53,73 @@ var displayImages = {
     }
 };
 
-/* Timeline that alternates between instructions and images */
-var ind = 0;
-
-
-
 var displayInstruction = {
     type: jsPsychHtmlButtonResponse,
-    stimulus: jsPsych.timelineVariable('stimulus'),
-    choices: ['Ok'],
+    stimulus: function() {return jsPsych.timelineVariable('stimulus')},
+    choices: function() { return [LabelNames.Next] },
     button_html: '<button class="jspsych-btn" style="font-size: 25px; left: 50%;">%choice%</button>'
   };
 
-
-  var test_procedure = {
+var test_procedure = {
     timeline: [displayInstruction, displayImages],
     timeline_variables: instructionStimuli
 }
 
-      var debrief_block = {
-        type: jsPsychHtmlKeyboardResponse,
-        stimulus: function() {
 
-          var trials = jsPsych.data.get().filter({task: 'response'});
-          var correct_trials = trials.filter({correct: true});
-          var accuracy = Math.round(correct_trials.count() / (trials.count()) * 100);
-          var rt = Math.round(correct_trials.select('rt').mean());
+/* Timeline that alternates between instructions and images */
+var ind = 0;
+var testInstructions = {
+    timeline: [displayInstruction,
+    //nested timeline to keep 'image screen' until 'Next' is clicked
+    {
+    timeline: [displayImages],
+    loop_function: function(data){
+      console.log('index is');
+      console.log(ind);
+      console.log('response is:');
+      console.log(data.values()[0].response);
+      console.log('correct response is:');
+      console.log(data.values()[0].correct_response[ind]);
+      // === 0 means Pencil
+      // === 1 means Next
+      // === 2 means Paper
+      if (data.values()[0].response === 1){
+        console.log('in if');
+        ind = 0;
+        return false;
+      } else {
+        console.log('not in if');
+        console.log(data.values()[0].response);
+        console.log(data.values()[0].correct_response[ind]);
+        console.log(data.values()[0].response === data.values()[0].correct_response[ind]);
+        ind++;
+        return true;
+      }
+    }
+  }],
+    timeline_variables: instructionStimuli,
+    randomize_order: false,
+  };
 
-          return `<p>You responded correctly on ${accuracy}% of the trials.</p>
-            <p>Your average response time was ${rt}ms.</p>
-            <p>Press any key to complete the experiment. Thank you!`;
+var debrief_block = {
+    type: jsPsychHtmlButtonResponse,
+    choices: ['Ok'],
+    stimulus: function() {
 
-        }
-      };
-      
+        var trials = jsPsych.data.get().filter({task: 'response'});
+        var correct_trials = trials.filter({correct: true});
+        var accuracy = Math.round(correct_trials.count() / (trials.count()) * 100);
+        var rt = Math.round(correct_trials.select('rt').mean());
 
+        return `<p>You responded correctly on ${accuracy}% of the trials.</p>
+        <p>Your average response time was ${rt}ms.</p>
+        <p>Press any key to complete the experiment. Thank you!`;
+    }
+};
+
+timeline.push(Welcome)
 timeline.push(preload);
 timeline.push(Instructions01)
-timeline.push(test_procedure);
+timeline.push(testInstructions);
 timeline.push(debrief_block);
+timeline.push(ThankYou)
