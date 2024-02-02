11f7c258b14cb6a15790888273effa013e31a680
Yara Elmasry <yelma017@uottawa.ca>
2024-02-02 13:46:13 -0500

    testing imp2

--- functions/Scoring/ImageCopy_Scoring.js
+++ functions/Scoring/ImageCopy_Scoring.js
@@ -1,4 +1,4 @@
-function ImageCopy_Scoring(data) {
+/* function ImageCopy_Scoring(data) {
 	Notes = data.filter({trial: 'Notes'})
 	trialData = data.filter({trial: 'Image Copy'}).trials[0]
 	Results = {}
@@ -7,10 +7,12 @@ function ImageCopy_Scoring(data) {
 	Results.PrimaryResults['Accuracy'] = -99
 	Results.AllResults = {}
 	Results.AllResults['Accuracy'] = -99
+
+	*/
 	
 
-	function calculateCubeScore(data) {
-		let score = 0;
+function calculateCubeScore(data) {
+	let score = 0;
 	  
 		// Hypothetical scoring based on cube features
 		for (let i = 0; i < data.length; i++) {
@@ -22,6 +24,7 @@ function ImageCopy_Scoring(data) {
 			}
 		  }
 		}
+		console.log(score)
 	  
 		return score;
 	  }
@@ -31,7 +34,7 @@ function ImageCopy_Scoring(data) {
 	  console.log(`Cube Score: ${cubeScore}`);
 
 
-
+/*
 	Results.AllResults['Response Time (ms)'] = trialData.rt
 	if ( Notes.trials.length > 0 )
 		{ Results.AllResults['Notes'] = Notes.trials[0].response.Notes }
@@ -40,6 +43,6 @@ function ImageCopy_Scoring(data) {
 	//Results.AllResults['Notes'] = Notes.trials[0].response.Notes
 	console.log(Results)
 	return Results
-}
+}*/
 
  
