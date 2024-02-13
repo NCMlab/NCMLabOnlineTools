6b2e734c17c6e95c035c60dbd04f71d43ba558a5
Yara Elmasry <yelma017@uottawa.ca>
2024-02-12 21:15:37 -0500

    cube scoring

--- functions/Scoring/ImageCopy_Scoring.js
+++ functions/Scoring/ImageCopy_Scoring.js
@@ -1,48 +1,43 @@
-/* function ImageCopy_Scoring(data) {
-	Notes = data.filter({trial: 'Notes'})
-	trialData = data.filter({trial: 'Image Copy'}).trials[0]
-	Results = {}
-	Results.PrimaryResults = {}
-	Results.PrimaryResults['ScoreName'] = 'Accuracy'
-	Results.PrimaryResults['Accuracy'] = -99
-	Results.AllResults = {}
-	Results.AllResults['Accuracy'] = -99
+function calculateScore(drawingData){
+	const numLines = drawingData.lines.length;
 
-	*/
-	
+	//defining scoring criteria 
+	const maxLines = 12; // max lines allowed for perfect score
+	const perfectScore = 10; //perfect score value
 
-function ImageCopy_Scoring(data) {
-	let score = 0;
-	  
-		// Hypothetical scoring based on cube features
-		for (let i = 0; i < data.length; i++) {
-		  for (let j = 0; j < data[i].length; j++) {
-			// Check for cube features 
-			if (data[i][j] === 1) {
-			  // might add more scoring logic based on specific req 
-			  score += 1;
-			}
-		  }
-		}
-		
-		console.log(score)
-		return score;
-	  }
-	  
-	  // Calculate and display the score for the cube image
-	  const cubeScore = ImageCopy_Scoring(cubeImage);
-	  console.log(`Cube Score: ${cubeScore}`);
+	//calculation of scores based on number of lines
+	//assigns a score if number of lines is within the limit 
+	let score; 
+	if (numLines <=maxLines){
+		score = perfectScore - (numLines/maxLines)* perfectScore;
+	}else {
+		//if number of lines exceeds max lines 
+		score =0 
+	}
 
+	//might add more criteria here..... TBD
+	return score
+}
 
-/*
-	Results.AllResults['Response Time (ms)'] = trialData.rt
-	if ( Notes.trials.length > 0 )
-		{ Results.AllResults['Notes'] = Notes.trials[0].response.Notes }
-	else { Results.AllResults['Notes'] = '' }
+//function to handle scoring at the end of the exeperiment 
 
-	//Results.AllResults['Notes'] = Notes.trials[0].response.Notes
-	console.log(Results)
-	return Results
-}*/
+function handleScoring(data){
+	//capture drawing data from experiment
+	const drawingData = data.values().next().value;
 
- 
+	//calculation of score based on drawing data
+	const score = calculateScore(drawingData)
+	return {score: score};
+}
+
+//updating send data component to include scoring information
+var SendData = {
+	type: jsPyschCallFunction,
+	func: function(){
+		var data = jsPysch.data.get();
+
+		var scoringResults = handleScoring(data);
+
+		jsPysch.finishTrial(scoringResults);
+	},
+};
