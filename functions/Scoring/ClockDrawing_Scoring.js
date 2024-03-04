1f1068d13613bec70941136e5475d5b8cb87efdb
Yara Elmasry <yelma017@uottawa.ca>
2024-03-04 15:48:34 -0500

    clock drawing scoring logic

--- functions/Scoring/ClockDrawing_Scoring.js
+++ functions/Scoring/ClockDrawing_Scoring.js
@@ -1,4 +1,80 @@
 function ClockDrawing_Scoring(data) {
+	// filtering data for clock
+    const clockDrawingTrialData = data.filter({trial: 'Clock Drawing'}).first();
+    const notesData = data.filter({trial: 'Notes'}).first();
+
+    // initialzing results object
+    let results = {
+        PrimaryResults: {
+            ScoreName: 'Accuracy',
+            Accuracy: -99 // default value if accuracy cannot be calculated
+        },
+        AllResults: {
+            Accuracy: -99, 
+            'Response Time (ms)': clockDrawingTrialData.rt || 0, // response time in milliseconds
+            Notes: notesData ? notesData.response.Notes : '' // notes if available
+        }
+    };
+
+    // check if clock drawing trial data is available
+    if (clockDrawingTrialData) {
+        // check if clock drawing response data is available
+        if (clockDrawingTrialData.response) {
+            // 1) we need to implement the accuracy calculation based on the user's clock drawing response
+            // we can compare the user's drawn clock with a reference clock and calculate accuracy
+            // and replace the below line with the accuracy calculation logic
+            const accuracy = calculateAccuracy(clockDrawingTrialData.response);
+
+            // update accuracy in results object
+            results.PrimaryResults.Accuracy = accuracy;
+            results.AllResults.Accuracy = accuracy;
+        }
+    }
+
+    return results;
+}
+
+// accuracy calculation function (not implemented yet)
+function calculateAccuracy(responseData) {
+    if (responseData.attempted) {
+        return 100; // Maximum accuracy
+    } else {
+        return 0; // Minimum accuracy
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function ClockDrawing_Scoring(data) {
 	Notes = data.filter({trial: 'Notes'})
 	trialData = data.filter({trial: 'Clock Drawing'}).trials[0]
 	console.log(trialData)
@@ -17,4 +93,4 @@ function ClockDrawing_Scoring(data) {
 	else { Results.AllResults['Notes'] = '' }
 
 	return Results
-}
+} */
