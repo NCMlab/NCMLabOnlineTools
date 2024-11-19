41e74df0970bb43ec0e921d92d1e284296ac22b1
Jason Steffener <steffener@gmail.com>
2024-11-19 14:00:49 -0500

    trying to put a slider  in

---
+++ Playground/json.js
@@ -0,0 +1,26 @@
+const json = {
+    elements: [
+      {
+        "type": "bootstrapslider",
+        "name": "bootstrapslider-widget",
+        //"step": 1,
+        //"rangeMin": 0,
+        //"rangeMax": 100,
+        "orientation": "vertical",
+        "ticks":[0,50,75,100],
+        "tick_labels":['0', '50', '100'],
+        "config": {
+            //"orientation": "vertical",
+            //"value":"10",
+            //"reversed":true,
+            "ticks":[0,50,75,100],
+            "tick_labels":['0', '50', '100'],
+            //"natural_arrow_keys":true
+
+            
+
+        }
+
+      }
+    ]
+  };
