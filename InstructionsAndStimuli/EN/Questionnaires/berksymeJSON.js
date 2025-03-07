baf70343cce36dc2a4769da12c7b094819002a84
ghaisramzi <ghais_lteif_06@hotmail.com>
2025-03-07 13:38:57 -0500

    edited english berksyme

--- InstructionsAndStimuli/EN/Questionnaires/berksymeJSON.js
+++ InstructionsAndStimuli/EN/Questionnaires/berksymeJSON.js
@@ -6,20 +6,28 @@ const survey_JSON = {
   pages: [{
     elements: [
 
+    {
+        type: "html",
+        name: "introduction",
+        html: "The following questions are about your social interactions. Please read the following questions and select the response that most closely describes your current situation."
+       },
 
       {
         type: "radiogroup",
         name: "berksyme001",
-        title: "In a typical week, how many times do you talk on the telephone with family, friends, or neighbours?",
+        title: "Which of these statements best represents your current relationship status?",
         isRequired: true,
         showNoneItem: false,
         showOtherItem: false,
         colCount: 1,
         choices: [ 
-          { value: 0, text: "Less than once a week" }, 
-          { value: 1, text: "1 or 2 times a week" }, 
-          { value: 2, text: "3 or 4 times a week" }, 
-          { value: 3, text: "5 or more times a week" } 
+          { value: 0, text: "Single" }, 
+          { value: 1, text: "Married" }, 
+          { value: 2, text: "In a civil union" }, 
+          { value: 3, text: "Divorced" }, 
+          { value: 4, text: "Widowed"}, 
+          { value: 5, text: "Seperated" }, 
+          { value: 6, text: "I prefer not to answer"} 
         ],
         separateSpecialChoices: true,
         showClearButton: true
@@ -27,16 +35,17 @@ const survey_JSON = {
       {
         type: "radiogroup",
         name: "berksyme002",
-        title: "How often do you get together with friends or relatives?",
+        title: "In the past two weeks, how often have you communicated with family (e.g., parent, partner, child, cousin), friends, or neighbors (e.g., phone calls, video calls, text messages, social media)?",
         isRequired: true,
         showNoneItem: false,
         showOtherItem: false,
         colCount: 1,
         choices: [ 
           { value: 0, text: "Less than once a week" }, 
-          { value: 1, text: "1 or 2 times a week" }, 
-          { value: 2, text: "3 or 4 times a week" }, 
-          { value: 3, text: "5 or more times a week" } 
+          { value: 1, text: "1 to 2 times a week" }, 
+          { value: 2, text: "3 to 4 times a week" }, 
+          { value: 3, text: "5 or more times a week" }, 
+          { value: 4, text: "I prefer not to answer"}
         ],
         separateSpecialChoices: true,
         showClearButton: true
@@ -44,51 +53,96 @@ const survey_JSON = {
       {
         type: "radiogroup",
         name: "berksyme003",
-        title: "How often do you attend church or religious services?",
+        title: "In the past two weeks, how often have you met up with or spent time with friends, family or relatives in person?",
         isRequired: true,
         showNoneItem: false,
         showOtherItem: false,
         colCount: 1,
         choices: [ 
           { value: 0, text: "Less than once a week" }, 
-          { value: 1, text: "1 or 2 times a week" }, 
-          { value: 2, text: "3 or 4 times a week" }, 
-          { value: 3, text: "5 or more times a week" } 
+          { value: 1, text: "1 to 2 times a week" }, 
+          { value: 2, text: "3 to 4 times a week" }, 
+          { value: 3, text: "5 or more times a week" }, 
+          { value: 4, text: "I prefer not to answer"}
         ],
         separateSpecialChoices: true,
         showClearButton: true
       },
       {
-        type: "radiogroup",
-        name: "berksyme004",
-        title: "How often do you attend meetings of the clubs or organizations you belong to?",
-        isRequired: true,
-        showNoneItem: false,
-        showOtherItem: false,
-        colCount: 1,
-        choices: [ 
-          { value: 0, text: "Less than once a week" }, 
-          { value: 1, text: "1 or 2 times a week" }, 
-          { value: 2, text: "3 or 4 times a week" }, 
-          { value: 3, text: "5 or more times a week" } 
-        ],
-        separateSpecialChoices: true,
-        showClearButton: true
+        type: "panel",
+        name: "berksyme004_panel",
+        title: "In the past two weeks, how often have you attended religious, spiritual, or philosophical activities (e.g., services, meditation sessions, Mass, parish activities, prayer groups.)?",
+        elements: [
+          { 
+            type: "radiogroup",
+            name: "berksyme004_inperson",
+            title: "In-person",
+            isRequired: true,
+            showNoneItem: false,
+            showOtherItem: false,
+            colCount: 1,
+            choices: [
+              { value: 0, text: "Never" }, 
+              { value: 1, text: "Once" }, 
+              { value: 2, text: "Twice" }, 
+              { value: 3, text: "Three or more times" },
+              { value: 4, text: "I prefer not to answer"}
+            ]
+          },
+          { type: "radiogroup",
+            name: "berksyme004_online",
+            title: "Online",
+            isRequired: true,
+            showNoneItem: false,
+            showOtherItem: false,
+            colCount: 1,
+            choices: [
+              { value: 0, text: "Never" }, 
+              { value: 1, text: "Once" }, 
+              { value: 2, text: "Twice" }, 
+              { value: 3, text: "Three or more times" },
+              { value: 4, text: "I prefer not to answer"}
+            ]
+          }
+        ]
       },
       {
-        type: "radiogroup",
-        name: "berksyme005",
-        title: "Are you married or live together with someone in a partnership?",
-        isRequired: true,
-        showNoneItem: false,
-        showOtherItem: false,
-        colCount: 1,
-        choices: [
-          { value: 1, text: "Yes" },
-          { value: 0, text: "No" }
-        ],
-        separateSpecialChoices: true,
-        showClearButton: true
+        type: "panel",
+        name: "berksyme005_panel",
+        title: "In the past two weeks, how often have you attended events or activities of any groups, clubs or organizations you belong to (e.g., hobby or sports groups, community activities, forums, clubs)?",
+        elements: [
+          { 
+            type: "radiogroup",
+            name: "berksyme005_inperson",
+            title: "In-person",
+            isRequired: true,
+            showNoneItem: false,
+            showOtherItem: false,
+            colCount: 1,
+            choices: [
+              { value: 0, text: "Never or less than once a week" }, 
+              { value: 1, text: "1 to 2 times a week" }, 
+              { value: 2, text: "3 to 4 times a week" }, 
+              { value: 3, text: "5 or more times a week" },
+              { value: 4, text: "I prefer not to answer"}
+            ]
+          },
+          { type: "radiogroup",
+            name: "berksyme005_online",
+            title: "Online",
+            isRequired: true,
+            showNoneItem: false,
+            showOtherItem: false,
+            colCount: 1,
+            choices: [
+              { value: 0, text: "Never or less than once a week" }, 
+              { value: 1, text: "1 to 2 times a week" }, 
+              { value: 2, text: "3 to 4 times a week" }, 
+              { value: 3, text: "5 or more times a week" },
+              { value: 4, text: "I prefer not to answer"}
+            ]
+          }
+        ]
       },
     ],
   }]
