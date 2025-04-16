f29515a78250a8e55f04083e9e78296df4a85a5e
Jason Steffener <steffener@gmail.com>
2025-04-16 16:07:33 -0400

    minor chnage the EQ5D that needs to be done propely at some point

--- InstructionsAndStimuli/Instructions/EQ5D_Instructions.js
+++ InstructionsAndStimuli/Instructions/EQ5D_Instructions.js
@@ -4,3 +4,8 @@ EN_Instructions.WelcomeText = [{'page': 'Please fill out the following survey.'}
 EN_Instructions.ThankYouText = [{'page': 'Thank you.'}]
 add('EN_Instructions_Default', function(){ Instructions = EN_Instructions});
 
+
+var FR_Instructions = {}
+FR_Instructions.WelcomeText = [{'page': 'Please fill out the following survey.'}]
+FR_Instructions.ThankYouText = [{'page': 'Thank you.'}]
+add('FR_Instructions_Default', function(){ Instructions = FR_Instructions});
