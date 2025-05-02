03929acb5b2bcb78db7796bf9cc6e0e5866ee9a0
Jason Steffener - laptop <steffener@gmail.com>
2025-05-02 14:30:33 -0400

    trying make header buttons more reliable

--- functions/GeneralPurposeFunctions/CommonParts.js
+++ functions/GeneralPurposeFunctions/CommonParts.js
@@ -25,12 +25,12 @@ function UpdateHeader() {
             //document.getElementById('header-stop-button').textContent = LabelNames.Stop;
             //document.getElementById('header-quit-button').textContent = LabelNames.Quit;
             //document.getElementById('header-skip-button').textContent = LabelNames.Skip;
-            document.getElementById('header-info-button').style.visibility = "hidden"
+            //document.getElementById('header-info-button').style.visibility = "hidden"
             
             //document.getElementById('header-home-button').style.visibility = "hidden"
-            document.getElementById('header-quit-button').style.visibility = "hidden"
-            document.getElementById('header-skip-button').style.visibility = "hidden"
-            document.getElementById('header-stop-button').style.visibility = "hidden"
+            //document.getElementById('header-quit-button').style.visibility = "hidden"
+            //document.getElementById('header-skip-button').style.visibility = "hidden"
+            //document.getElementById('header-stop-button').style.visibility = "hidden"
         } catch (error) {
             console.log(error);
         }
