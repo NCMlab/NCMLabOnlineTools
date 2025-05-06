2fbb6f6f31288e360080cca5e8c8e200cacfd0b1
Jason Steffener <steffener@gmail.com>
2025-05-06 09:33:42 -0400

    Made an updatred version of Questionnaire.html that uses promises (its better!)

--- functions/GeneralPurposeFunctions/HeaderFunctions.js
+++ functions/GeneralPurposeFunctions/HeaderFunctions.js
@@ -1,13 +1,7 @@
 
 function UpdateHeader() {
-    //hide  the information button for now
-    //
-    // set the header button language
-    //console.log(document.getElementById('header-home-button'))
-   
-   // window.onload = function() {
         try {
-            
+            // set the language of the header buttons
             document.getElementById('header-home-button').textContent = LabelNames.Home;
             document.getElementById('header-restart-button').textContent = LabelNames.Restart;
             document.getElementById('header-stop-button').textContent = LabelNames.Stop;
@@ -15,43 +9,23 @@ function UpdateHeader() {
             document.getElementById('header-skip-button').textContent = LabelNames.Skip;
             document.getElementById('header-info-button').style.visibility = "hidden"
             
+            // set the visibility of the header buttons
             //document.getElementById('header-home-button').style.visibility = "hidden"
             document.getElementById('header-quit-button').style.visibility = "hidden"
             document.getElementById('header-restart-button').style.visibility = "hidden"
             document.getElementById('header-skip-button').style.visibility = "hidden"
             document.getElementById('header-stop-button').style.visibility = "hidden"
+            
+            // set the language pulldown button             
+            var LangIsSetTo = jatos.batchSession.get(jatos.workerId+"_Language")
+            //jatos.batchSession.set(jatos.workerId+"_Language", sel.options[sel.selectedIndex].text) 
+            var elem = document.getElementById('LanguageSelectPulldown')
+            elem.value = LangIsSetTo
+   
+        
         } catch (error) {
             console.log(error);
         }
             // Check to see if we are in the User Choice screen, if so turn off restart and home
 
-        
-   // }
-
-            
-    
-}
-function SetLanguagePulldown() {
-    //window.onload = function() {
-        // Has the language already been selected?
-        console.log(jatos.batchSession.getAll())
-        var LangIsSetTo = jatos.batchSession.get(jatos.workerId+"_Language")
-        //jatos.batchSession.set(jatos.workerId+"_Language", sel.options[sel.selectedIndex].text) 
-        console.log("LANGUAGE IS SET TO: "+LangIsSetTo) 
-        console.log("Current worker is: "+jatos.workerId)
-        console.log(jatos.batchSession.getAll())
-        var elem = document.getElementById('LanguageSelectPulldown')
-        console.log(elem)
-        console.log(elem.value)
-        elem.value = LangIsSetTo
-        console.log(elem.value)
-    //}
-}
-function GetLanguagePulldown() {
-    //window.onload = function() {
-        var elem = document.getElementById('LanguageSelectPulldown')
-        console.log(elem)
-        console.log(elem.value)
-
-    //}
-}
+    }
