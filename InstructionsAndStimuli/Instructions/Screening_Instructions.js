faf8f93146f121637a61a5d410f3610b75c5f245
Jason Steffener - laptop <steffener@gmail.com>
2026-01-23 14:21:31 -0500

    update to screening

--- InstructionsAndStimuli/Instructions/Screening_Instructions.js
+++ InstructionsAndStimuli/Instructions/Screening_Instructions.js
@@ -1,7 +1,19 @@
 var EN_Instructions_Default = {}
 EN_Instructions_Default.Instructions01 = 
 [
-    {'page':'There are buttons in the upper left hand corner. The Stop button allows you to stop the experiment but to keep your data. The Quit button allows you to leave the experiment and have your data removed.'},
+    {'page': '<p>The purpose of this study is to study the effects of aging on memory using online browser based tools. This includes tasks the require listening, speaking and drawing. To do this you will need to use a computer or tablet with a microphone and speakers.</p>'+ 
+    '<p>The steps include:</p><ul>'+
+    '<li>A brief screening for eligibility</li>'+
+    '<li>Reading and providing informed consent</li>'+
+    '<li>Completing a questionnaire</li>'+
+    '<li>Testing your speakers</li>'+
+    '<li>Testing your microphone</li>'+
+    '<li>A task where you listen to lists of words and repeat them back</li>'+
+    '<li>A task where you sort images</li>'+
+    '<li>Two drawing tasks, similair to "connect the dots"</li>'+
+    '<li>Two brief tasks based on your memory of the words you listened to</li></ul>' 
+    },
+    {'page':'There are buttons in the upper left hand corner. The <em>Stop</em> button allows you to stop the experiment but to keep your data. The <em>Quit</em> button allows you to leave the experiment and have your data removed.'},
     {'page':'When you press next you will start with a screening form.'}
 ]
 add('EN_Instructions_Default', function() {Instructions = EN_Instructions_Default});
