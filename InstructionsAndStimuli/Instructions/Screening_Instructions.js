f819d8209970e97c5fdaa64f06a790f6c8f8a3c8
Jason Steffener - laptop <steffener@gmail.com>
2026-01-26 11:51:23 -0500

    updated FR screening

--- InstructionsAndStimuli/Instructions/Screening_Instructions.js
+++ InstructionsAndStimuli/Instructions/Screening_Instructions.js
@@ -1,19 +1,20 @@
 var EN_Instructions_Default = {}
 EN_Instructions_Default.Instructions01 = 
 [
-    {'page': '<p>The purpose of this study is to study the effects of aging on memory '+
-        'using online browser based tools. This includes tasks the require listening, speaking'+
-        ' and drawing. To do this you will need to use a computer or tablet with a microphone '+
-        'and speakers. This will take between 20 and 30 minutes.</p>'+ 
+    {'page': '<p>The purpose of this study is to study the effects of aging on '+
+        'memory using online browser-based tools. This includes tasks that require '+
+        'listening, speaking, and drawing. To do this, you will need to use a computer'+
+        ' or tablet with a microphone and speakers, not a cell phone. This will take '+
+        'between 20 and 30 minutes.</p>'+ 
     '<p>The steps include:</p><ul>'+
-    '<li>A brief screening for eligibility</li>'+
-    '<li>Reading and providing informed consent</li>'+
-    '<li>Completing a questionnaire</li>'+
-    '<li>Testing your speakers</li>'+
-    '<li>Testing your microphone</li>'+
-    '<li>A task where you listen to lists of words and repeat them back</li>'+
-    '<li>A task where you sort images</li>'+
-    '<li>Two drawing tasks, similair to "connect the dots"</li>'+
+    '<li>A brief screening for eligibility.</li>'+
+    '<li>Reading and providing informed consent.</li>'+
+    '<li>Completing a brief questionnaire.</li>'+
+    '<li>Testing your speakers.</li>'+
+    '<li>Testing your microphone.</li>'+
+    '<li>A task where you listen to lists of words and repeat them back.</li>'+
+    '<li>A task where you sort cards.</li>'+
+    '<li>Two drawing tasks, similair to "connect the dots."</li>'+
     '<li>Two brief tasks based on your memory of the words you listened to.</li></ul>' 
     },
     {'page':'There are buttons in the upper left hand corner. The <em>Stop</em> button allows you to stop the experiment but to keep your data. The <em>Quit</em> button allows you to leave the experiment and have your data removed.'},
@@ -24,6 +25,22 @@ add('EN_Instructions_Default', function() {Instructions = EN_Instructions_Defaul
 var FR_Instructions_Default = {}
 FR_Instructions_Default.Instructions01 = 
 [
+    {'page': "<p>L'objectif de cette étude est d'étudier les effets du vieillissement sur la "+
+        "mémoire à l'aide d'outils en ligne accessibles via un navigateur. Cela comprend des "+
+        "tâches qui nécessitent d'écouter, de parler et de dessiner. Pour ce faire, vous devrez "+
+        "utiliser un ordinateur ou une tablette équipé d'un microphone et de haut-parleurs, et "+
+        "non un téléphone portable. Cela prendra entre 20 et 30 minutes.</p>"+
+        "<p>Les étapes sont les suivantes</p><ul>"+
+        "<li>Une brève sélection pour vérifier votre éligibilité."+
+        "<li>La lecture et la signature d'un formulaire de consentement éclairé."+
+        "<li>La réponse à un bref questionnaire."+
+        "<li>Le test de vos haut-parleurs."+
+        "<li>Le test de votre microphone."+
+        "<li>Une tâche consistant à écouter des listes de mots et à les répéter."+
+        "<li>Une tâche consistant à trier des cartes."+
+        "<li>Deux tâches de dessin, similaires à « relier les points »."+
+        "<li>Deux tâches brèves basées sur votre mémoire des mots que vous avez écoutés."
+    },
     {'page':'Des boutons sont disponibles en haut à gauche. Le bouton Arrêt vous permet d\'arrêter l\'expérience tout en conservant vos données. Le bouton Quitter vous permet de quitter l\'expérience et de supprimer vos données.'},
     {'page':'Lorsque vous appuyez sur Suivant, vous commencerez avec un formulaire de sélection.'}
 ]
