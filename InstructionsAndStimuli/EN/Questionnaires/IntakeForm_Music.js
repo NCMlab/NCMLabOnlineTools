a671f6b557462b6b4eb30cc6e9368c13cdb51791
Jason Steffener - laptop <steffener@gmail.com>
2026-04-17 08:29:46 -0400

    put the intakw questionnaire back

--- InstructionsAndStimuli/EN/Questionnaires/IntakeForm_Music.js
+++ InstructionsAndStimuli/EN/Questionnaires/IntakeForm_Music.js
@@ -19,7 +19,7 @@ const json = {
       title: "Demographic Information",
        elements: [
      
-       /* {
+        {
           type: 'dropdown',
           title: "Gender", 
           name: 'dem001_gender', 
@@ -154,7 +154,7 @@ const json = {
           title: "Please specify your language or communication challenges.",
           visibleIf: "{dem009_langProb} == Yes",
           maxLength: 500
-        },*/
+        },
 // ======== DOES THIS QUESTION GET RECORDED????? ============
         {
           type: "tagbox",
@@ -173,7 +173,7 @@ const json = {
           "description": "Please select all that apply.",
           isRequired: true
         },
-/*        {
+        {
           type: 'dropdown',
           title: 'What is your marital status?',
           choices: 
@@ -215,7 +215,7 @@ const json = {
           ],
           name: 'dem013_pastNeed',
           isRequired: true,
-       }*/
+       }
        ]
      },
      {
